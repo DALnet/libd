be50b1ff505bcb34483351ec76072e3e92f8166d
epiphani <epiphani@dal.net>
2009-02-28 12:41:11 -0500

    Add intermediate layer of socket abstraction before the socket interactions themselves.

---
+++ src/mfd.c
@@ -0,0 +1,60 @@
+/* mfd.c
+ * Manage file descriptor abstraction
+ */
+
+#include "sockeng.h"
+
+static void fd_assert(SockEng *s, myfd *fd)
+{
+#ifdef DEBUG
+	if(fd->fd < 0 || fd->fd > MAX_FDS)
+		abort();
+	if(s->local[fd->fd] != fd)
+		abort();
+	if(fd->owner == NULL)
+		abort();
+	if(fd->cb == NULL)
+		abort();
+#endif
+}
+
+
+int mfd_add(SockEng *s, myfd *fd, void *owner, void (*cb)())
+{
+	if(s->local[fd->fd] != NULL)
+		return -1;
+	fd->owner = owner;
+	fd->cb = cb;
+	fd->state = 0;
+	s->local[fd->fd] = fd;
+
+	return 0;
+}
+
+void mfd_del(SockEng *s, myfd *fd)
+{
+	fd_assert(s, fd);
+	s->local[fd->fd] = NULL;
+}
+
+void mfd_read(SockEng *s, myfd *fd)
+{
+	fd_assert(s, fd);
+	fd->state |= MFD_READ;
+}
+
+void mfd_write(SockEng *s, myfd *fd)
+{
+	fd_assert(s, fd);
+	fd->state |= MFD_WRITE;
+}
+
+void mfd_set_internal(SockEng *s, int fd, void *ptr)
+{
+	s->local[fd]->internal = ptr;
+}
+
+void *mfd_get_internal(SockEng *s, int fd)
+{
+	return s->local[fd]->internal;
+}
